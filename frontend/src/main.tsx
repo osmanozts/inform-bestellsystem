5c0dddbac45c84d65dc166e091180b0db6276ab8
osman.ozts <osman.ozts@gmail.com>
2026-08-16 12:47:02 +0200

    feature-based folder skeleton with layer responsibilities

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
-import App from './App.tsx';
+import { App } from './app/index.ts';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
